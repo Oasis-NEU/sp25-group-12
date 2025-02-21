8e92ece21ce7455a9d23a8883033bd37b20991a7
minfys <ashleyminu@yahoo.com>
2025-02-21 11:13:35 -0500

    added sidebar

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,21 @@
+import './Sidebar.css';
+
+function Sidebar() {
+    return (
+        <div className="navbar">
+            <a href="#home">Home</a>
+            <a href="#course1">Course 1</a>
+            <a href="#course2">Course 2</a>
+            <a href="#course3">Course 3</a>
+            <button className="dropdown-btn">
+                Study Methods
+                <i className="study-methods"></i>
+            </button>
+            <div className="dropdown-container">
+                <a href="#pomodoro">Pomodoro</a>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar;
