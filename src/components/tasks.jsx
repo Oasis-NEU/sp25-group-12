5c05899276cb0eac2781728cb26f1ad2e34169d8
visqe1 <emilyn1806@gmail.com>
2025-02-22 02:07:12 -0500

    Fix dots and date alignment (and maybe checkbox size issue?

--- src/components/tasks.jsx
+++ src/components/tasks.jsx
@@ -8,62 +8,62 @@ function Tasks() {
                 <li>
                     <input type="checkbox" />
                     <span className="task-name">First</span>
-                    <span className="dots">................</span>
+                    <span className="dots"></span>
                     <span className="task-date">3/05/25</span>
                 </li>
                 <li>
                     <input type="checkbox" />
                     <span className="task-name">Second</span>
-                    <span className="dots">................</span>
-                    <span className="task-date">3/15/25</span>
+                    <span className="dots"></span>
+                    <span className="task-date">3/05/25</span>
                 </li>
                 <li>
                     <input type="checkbox" />
                     <span className="task-name">Third</span>
-                    <span className="dots">................</span>
-                    <span className="task-date">3/15/25</span>
+                    <span className="dots"></span>
+                    <span className="task-date">3/05/25</span>
                 </li>
                 <li>
                     <input type="checkbox" />
                     <span className="task-name">Fourth</span>
-                    <span className="dots">................</span>
-                    <span className="task-date">3/15/25</span>
+                    <span className="dots"></span>
+                    <span className="task-date">3/05/25</span>
                 </li>
                 <li>
                     <input type="checkbox" />
-                    <span className="task-name">Second</span>
-                    <span className="dots">................</span>
-                    <span className="task-date">3/15/25</span>
+                    <span className="task-name">One Two Three</span>
+                    <span className="dots"></span>
+                    <span className="task-date">3/05/25</span>
                 </li>
                 <li>
                     <input type="checkbox" />
-                    <span className="task-name">Second</span>
-                    <span className="dots">................</span>
-                    <span className="task-date">3/15/25</span>
+                    <span className="task-name">One Two Three Four</span>
+                    <span className="dots"></span>
+                    <span className="task-date">3/05/25</span>
                 </li>
                 <li>
                     <input type="checkbox" />
-                    <span className="task-name">Second</span>
-                    <span className="dots">................</span>
-                    <span className="task-date">3/15/25</span>
+                    <span className="task-name">Task</span>
+                    <span className="dots"></span>
+                    <span className="task-date">3/05/25</span>
                 </li>
                 <li>
                     <input type="checkbox" />
-                    <span className="task-name">Second</span>
-                    <span className="dots">................</span>
-                    <span className="task-date">3/15/25</span>
+                    <span className="task-name">Task</span>
+                    <span className="dots"></span>
+                    <span className="task-date">3/05/25</span>
                 </li>
                 <li>
                     <input type="checkbox" />
-                    <span className="task-name">Second</span>
-                    <span className="dots">................</span>
-                    <span className="task-date">3/15/25</span>
+                    <span className="task-name">Task</span>
+                    <span className="dots"></span>
+                    <span className="task-date">3/05/25</span>
                 </li>
                 <li>
                     <input type="checkbox" />
-                    <span className="task-name">Second</span>
-                    <span className="dots">................</span>
-                    <span className="task-date">3/15/25</span>
+                    <span className="task-name">Task</span>
+                    <span className="dots"></span>
+                    <span className="task-date">3/05/25</span>
                 </li>
             </ul>
         </div>
